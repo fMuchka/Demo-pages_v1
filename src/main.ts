b59ff6c9fccfe335fc2f5dae85bb33df0b93cff3
fMuchka <f.muchka@gmail.com>
2022-02-13 17:21:27 +0100

    animate on scroll

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import Vue from "vue";
+import AOS from "aos"
+import "aos/dist/aos.css"
+
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
@@ -8,5 +11,8 @@ new Vue({
     router,
     store,
     vuetify,
-    render: (h) => h(App)
+    render: (h) => h(App),
+    mounted: function(){
+        AOS.init()
+    }
 }).$mount('#app');
